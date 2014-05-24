49050bc424e6f1773915f6875682c7d6e1f16ec2
ligi <ligi@ligi.de>
2014-05-24 18:20:05 +0200

    update to latest versions

--- src/main/java/org/ligi/gradecoba/ArtifactProvider.java
+++ src/main/java/org/ligi/gradecoba/ArtifactProvider.java
@@ -9,12 +9,12 @@ public class ArtifactProvider {
 	public List<Artifact> getArtifacts() {
 		if (artifacts==null) {
             artifacts = new ArrayList<Artifact>();
-			artifacts.add(new GoogleArtifact("play-services","4.0.30","google",false));
-			artifacts.add(new AndroidArtifact("gridlayout-v7","18.0.0","android-support",false));
-			artifacts.add(new AndroidArtifact("support-v13","19.0.0","android-support",false));
-			artifacts.add(new AndroidArtifact("mediarouter-v7","19.0.0","android-support",false));
-			artifacts.add(new AndroidArtifact("appcompat-v7","19.0.0","android-support",false));
-			artifacts.add(new AndroidArtifact("support-v4","19.0.0","android-support",false));
+			artifacts.add(new GoogleArtifact("play-services","4.3.23","google",false));
+			artifacts.add(new AndroidArtifact("gridlayout-v7","19.1.0","android-support",false));
+			artifacts.add(new AndroidArtifact("support-v13","19.1.0","android-support",false));
+			artifacts.add(new AndroidArtifact("mediarouter-v7","19.1.0","android-support",false));
+			artifacts.add(new AndroidArtifact("appcompat-v7","19.1.0","android-support",false));
+			artifacts.add(new AndroidArtifact("support-v4","19.1.0","android-support",false));
 		}
 		
 		return artifacts; 
